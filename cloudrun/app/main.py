0150eb809f1f9ddbf0da67089c04b86f7d4dbb3b
heiwado-ta-yoshida <120167184+heiwado-ta-yoshida@users.noreply.github.com>
2024-06-13 18:17:13 +0900

    修正

---
+++ cloudrun/app/main.py
@@ -0,0 +1,16 @@
+import  logging
+
+# ロガー設定
+google.cloud.logging.Client().setup_logging()
+
+@app.post(f"/")
+def output_log():
+
+    logging.info(f"[log]:start")
+
+    try:
+        logging.info(f"[log]:hogehoge")
+        raise ExceptionError
+
+    except ExceptionError:
+        logging.error(f"[log]:error!!")
--- cloudrun/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-import routers
-
-
-app = FastAPI()
-
-app.include_router(routers.a16_1hogebat.router)
