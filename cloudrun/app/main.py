f40708a420f9b5f1e69683b4eb6a5c7834717ccc
heiwado-ta-yoshida <120167184+heiwado-ta-yoshida@users.noreply.github.com>
2024-07-23 11:51:27 +0900

    修正

--- cloudrun/app/main.py
+++ cloudrun/app/main.py
@@ -5,10 +5,11 @@ import logging
 # google.cloud.logging.Client().setup_logging()
 
 app = FastAPI()
+log = "hogehoge"
 
 @app.get(f"/")
 def home():
-    logging.info(f"[log]:start")
+    logging.info(f"[{log}]:start")
     return 'Hello World!'
 
 if __name__ == "__main__":
